529934959a42f3838e316cd1fd44710c64261974
Víctor <victor.arranz@gruposantander.com>
2023-01-10 12:42:31 +0100

    html file spring

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DemoApplication {
+    
+    @RequestMapping("/")
+    public String home(){
+        return "Hola Jenkins!";
+    }
 
-	public static void main(String[] args) {
-            SpringApplication.run(DemoApplication.class, args);
-            System.out.println("Hola Jenkins!");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("Hola Jenkins!");
+    }
 
 }
